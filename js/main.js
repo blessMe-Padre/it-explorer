329c748c4a0a16175c23863b5430d40c06af25d6
Сергей Субботин <zarodiny@yandex.ru>
2024-01-04 17:41:38 +1000

    feat: добавляет форму и попап

--- js/main.js
+++ js/main.js
@@ -3,15 +3,21 @@ import { initSlider } from "./module/sliders.js";
 import { initCurrentYear } from "./module/current-year.js";
 import { initScrollToTop } from "./module/scroll-to-top.js";
 import { initScroll } from "./module/scroll.js";
+import { initPopup } from "./module/popup.js";
+import { initMask } from "./module/mask.js";
+import { initValidation } from "./module/validation.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('подключен скрипт main.js');
 
     initNav();
+    initMask();
+    initValidation();
     // initSlider();
     // initCurrentYear();
     // initScrollToTop();
     // initScroll();
     // baguetteBox.run('.gallery-wrapper');
+    initPopup();
 
 });
