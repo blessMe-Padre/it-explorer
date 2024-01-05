7565c1ada03bbab7e05b2a45b1092958bf34c373
Сергей Субботин <zarodiny@yandex.ru>
2024-01-05 20:57:21 +1000

    feat: добавляет форму

--- js/module/validation.js
+++ js/module/validation.js
@@ -57,21 +57,21 @@ const initValidation = () => {
         form.querySelectorAll('input').forEach(input => {
             input.addEventListener('input', () => {
                 if (input.type === 'tel' && input.value.length >= 18) {
-                    input.style.border = "2px solid rgb(0, 196, 0)";
+                    input.style.border = "1px solid rgb(0, 196, 0)";
                     input.classList.remove('_error');
                 } else if (input.type === 'tel' && input.value.length <= 4) {
-                    input.style.border = "2px solid transparent";
+                    input.style.border = "1px solid #ffffff";
                     input.classList.remove('_error');
                 } else if (input.type === 'tel' && input.value.length > 4 && input.value.length <= 17) {
-                    input.style.border = "2px solid red";
+                    input.style.border = "1px solid red";
                 } else if (input.type === 'text' && input.value.length > 2) {
                     input.classList.remove('_error');
-                    input.style.border = "2px solid rgb(0, 196, 0)";
+                    input.style.border = "1px solid rgb(0, 196, 0)";
                     input.style.boxShadow = "none";
                 } else if (input.type === 'text' && input.value.length === 0) {
                     input.classList.remove('_error');
                     input.style.boxShadow = "none";
-                    input.style.border = "2px solid transparent";
+                    input.style.border = "1px solid #ffffff";
                 }
             });
         });
@@ -98,6 +98,41 @@ const initValidation = () => {
 
     function emailTest2(input) {
         return /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/.test(input.value);
+    }
+
+    // ========================================================================================
+    // добавление файла
+
+    const formImage = document.querySelector('#formImage');
+    const formPreview = document.querySelector('#formPreview');
+    const formButton = document.querySelector('.file__button');
+
+    formImage.addEventListener('change', () => {
+        uploadFile(formImage.files[0]);
+        console.log(formImage.files[0].name);
+    });
+
+    const uploadFile = (file) => {
+        // проверяем тип файла
+        if (!['image/jpeg', 'image/png', 'image/gif', 'application/pdf'].includes(file.type)) {
+            alert('допустимы только изображения');
+            formImage.value = "";
+            return;
+        }
+        // проверяем размер (должен быть меньше <2mb)
+        if (file.size > 2 * 1024 * 1024) {
+            alert('файл должен быть меньше 2 мб');
+            return;
+        }
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            formButton.innerHTML = formImage.files[0].name;
+            // formPreview.innerHTML = `<img src="${e.target.result}" alt="foto">`;
+        }
+        reader.onerror = (e) => {
+            alert('ошибка');
+        }
+        reader.readAsDataURL(file);
     }
 
 
